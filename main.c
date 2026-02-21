9e5ffa6fe945ba2eafdeb56dbf61e87075ee5c52
edrf0 <125705907+edrf0@users.noreply.github.com>
2026-02-21 12:04:37 -0400

    .

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 int main(const int argc, const char *argv[])
 {
+    // argument count is 1 meaning no flags or 2+ arguments with the second being the -h flag
     if (argc == 1 || strcmp(argv[1], "-h") == 0) {
         printf("Usage:\n");
         printf("  Compress -c <path>\n");
@@ -13,24 +14,35 @@ int main(const int argc, const char *argv[])
         printf("  Help: -h\n");
         return 0;
     }
+    // argument count is 2 which necessarily fails due to lacking a file path or being unknown
     if (argc == 2) {
         fprintf(stderr, "Error: Missing file path argument.\n");
         return 1;
     }
+    // argument count is 3 which is ideal, next checks will confirm if flag is known
     if (argc == 3) {
+        // checking if flag is for compress
         if (strcmp(argv[1], "-c") == 0) {
             printf("Mode: Compression\n");
             printf("Target File: %s\n", argv[2]);
             compress(argv[2]);
         }
+        // checking for decompress flag
         else if (strcmp(argv[1], "-d") == 0) {
             printf("Mode: Decompression\n");
             printf("Target File: %s\n", argv[2]);
             decompress(argv[2]);
         }
-    } else {
-        fprintf(stderr, "Error: Unknown argument '%s'. Use -h for help.\n", argv[1]);
+        // argument count is right but flag is unknown
+        else {
+            fprintf(stderr, "Error: Unknown flag '%s'. Use -h for help.\n", argv[1]);
+            return 1;
+        }
+    } // incorrect number of arguments
+    else {
+        fprintf(stderr, "Error: Too many arguments Use -h for help.\n");
         return 1;
     }
+    // returning success code
     return 0;
 }
